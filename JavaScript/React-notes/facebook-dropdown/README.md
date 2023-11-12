ba1b3011ee89bb92e01059ee3f139629793dfda2
Kevin Nguyen <KNguyenSky@gmail.com>
2023-11-11 20:56:31 -0500

    Figured out react svgs
    Still taking notes on fireship's video, but we found out how to import svgs as react components

--- JavaScript/React-notes/facebook-dropdown/README.md
+++ JavaScript/React-notes/facebook-dropdown/README.md
@@ -3,3 +3,19 @@
 *   Just a dropdown and some notes about basic react concepts such
     as component composition, but also the way to do animations/transitions
     in React.
+
+# How to use svgs in React
+
+1. Import the svgs you want to use to the file as
+   "React Components". First convert them into react components
+2. Do 'npm install vite-plugin-svgr'
+3. Add vite plugin to vite.config.js
+4. Now you can import your svgs files as "react components".
+   Do 'import MySvgIcon from "./iconPath?react" to import
+   it. It used to be 'ReactComponent' but now it's this.
+
+5. Now we can use svgs as react components, but really they're
+   still svg tags on the inside, it's just that this way we didn't
+   have to do as much work.
+
+Timestamp: 07:26
--- JavaScript/React-notes/facebook-dropdown/src/components/App.jsx
+++ JavaScript/React-notes/facebook-dropdown/src/components/App.jsx
@@ -1,18 +1,41 @@
 import "../styles/App.css"
 
-function Navbar() {
+//
+import ArrowIcon from "../assets/arrow.svg?react"
+import BellIcon from "../assets/bell.svg?react"
+import BoltIcon from "../assets/bolt.svg?react"
+import CaretIcon from "../assets/caret.svg?react"
+import ChevronIcon from "../assets/chevron.svg?react"
+import CogIcon from "../assets/cog.svg?react"
+import MessengerIcon from "../assets/messenger.svg?react"
+import PlusIcon from "../assets/Plus.svg?react"
+
+function Navbar(props) {
     return (
         <nav className="navbar">
-            <ul className="navbar-nav"></ul>
+            <ul className="navbar-nav">{props.children}</ul>
         </nav>
+    )
+}
+
+function NavItem({ icon }) {
+    return (
+        <li className="nav-item">
+            <a href="#" className="icon-button">
+                {icon}
+            </a>
+        </li>
     )
 }
 
 function App() {
     return (
         <>
-            <Navbar></Navbar>
-            <h1>Hi</h1>
+            <Navbar>
+                <NavItem icon={<PlusIcon />}></NavItem>
+                <NavItem icon={<BellIcon />}></NavItem>
+                <NavItem icon={<MessengerIcon />}></NavItem>
+            </Navbar>
         </>
     )
 }
